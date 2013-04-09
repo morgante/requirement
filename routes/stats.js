ebae1f9f14c87baac4b2e68ea8ead5e38c0cd7f5
Morgante Pell <morgante.pell@morgante.net>
2013-04-10 00:50:05 +0400

    users

--- routes/stats.js
+++ routes/stats.js
@@ -104,12 +104,12 @@ exports.index = function( req, res ) {
 	
 	fillUsers();
 	
-	User.find({ 'disabled': { '$not': true } })
+	User.find({})
 			.sort( { 'fbPosts': 'desc' } )
 			.limit(10)
 			.exec( function( err, requesters ) {
 																				
-				User.find({ 'disabled': { '$not': true } })
+				User.find({})
 						.sort( { 'fbComments': 'desc' } )
 						.limit(10)
 						.exec( function( err, commentors ) {
