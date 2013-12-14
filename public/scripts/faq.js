8eef82a4e77279334892044b91960cef3b155ea2
Morgante Pell <morgante.pell@morgante.net>
2013-12-14 02:32:47 -0500

    yay search yay

---
+++ public/scripts/faq.js
@@ -0,0 +1,32 @@
+(function($) {
+	var $search;
+	var $questions;
+	var $answers;
+
+	var init = function() {
+		if ($('#faq').length < 1) {
+			return;
+		}
+
+		$search = $('#search');
+		$questions = $('.question');
+		$answers = $('.answer');
+
+		$search.keyup(function(err, key) {
+			var val = $search.val();
+
+			if (val.length > 0) {
+				$questions.hide();
+				$questions.filter(':contains("' + val + '")').show();
+
+				$questions.filter(':visible').children('.answer').collapse('show');
+				$questions.filter(':hidden').children('.answer').collapse('hide');
+			} else {
+				$questions.show();
+				$questions.children('.answer').collapse('hide');
+			}
+		});
+	};
+
+	$(init);
+}(jQuery));
