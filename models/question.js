53d04f679d8b0b1d33e3cad4dde1d38c17601b3b
Morgante Pell <morgante.pell@morgante.net>
2013-12-14 01:40:28 -0500

    woot woot

--- models/question.js
+++ models/question.js
@@ -1,5 +1,7 @@
 // we need mongoose
 var mongoose = require('mongoose');
+var natural = require('natural');
+var _ = require('underscore');
 
 var schema = mongoose.Schema({
 	question: String,
@@ -7,20 +9,56 @@ var schema = mongoose.Schema({
 	posts: [mongoose.Schema.Types.ObjectId]
 });
 
+var stemmer = natural.PorterStemmer;
+
+var compare = {
+	simplify: function(string) {
+		var stems = stemmer.tokenizeAndStem(string);
+		return stems;
+	},
+	score: function(str1, str2) {
+		stem1 = compare.simplify(str1);
+		stem2 = compare.simplify(str2);
+
+		return _.intersection(stem1, stem2).length;
+
+		return 0.5;
+	}
+}
+
 /**
  * Find a matching question for given text
  *
  * @param {Post} post The post to find a match for
- * @param {number} score The minimum matching score (0 to 1)
+ * @param {number} score The minimum matching score
  * @param {function} callback Function(err, question)
  */
-schema.static('findMatch', function (post, score, callback) {
+schema.static('findMatch', function (post, minScore, callback) {
 	if (post.question) {
 		this.findOne({_id: post.question}, function(err, question) {
 			callback(err, question);
 		});
 	} else {
-		callback(null, null);
+		// TODO: be more efficient
+		this.find({}, function(err, questions) {
+			if (err) {
+				callback(err, null);
+			} else {
+				var match = null;
+				questions.forEach(function(question) {
+					var score = compare.score(post.message, question.question);
+					if (score >= minScore) {
+						if (match == null || score >= match.score) {
+							match = {
+								score: score,
+								question: question
+							};
+						}
+					}
+				});
+				callback(null, match.question);
+			}
+		});
 	}
 });
 
